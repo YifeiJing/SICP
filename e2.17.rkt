a8711933619d9e197af3f8d2218e3e2167ca48f5
Yifei Jing <704001102@qq.com>
2019-05-28 19:39:11 +0800

    add ignore file

--- e2.17.rkt
+++ e2.17.rkt
@@ -1,7 +1,8 @@
 #lang sicp
+; Return the list that contains only the last element
 (define (last-pair list)
   (if (null? (cdr list))
-      (car list)
+      list
       (last-pair (cdr list))))
 (define ll (list 1 2 3 4))
-(last-pair ll)
+(display (last-pair ll))
